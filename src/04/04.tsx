303605e2d441f7e965296d3c79c890271e5d1ecf
Angor78 <angor78@gmail.com>
2022-05-28 18:14:34 +0300

    Destructure

--- src/04/04.tsx
+++ src/04/04.tsx
@@ -1,36 +1,47 @@
-import {StudentsType} from "./04.test";
-
-const ages = [22, 23, 45, 1, 91, 100]
-
-const predicat = (age: number) => {
-  return age > 90
-}
-const oldAges = [100]
-
-export type CoursersType = {
-  title: string
-  price: number
-}
-let courses = [
-  {title: "css", price: 100},
-  {title: "JS", price: 200},
-  {title: "React", price: 160},
-  {title: "Python", price: 100}
-]
-
-export const myMap = (array: StudentsType[], callback: any) => {
-  const newArray = []
-  for (let i = 0; i < array.length; i++) {
-    newArray[i] = callback(array[i])
-  }
-  return newArray
-}
-export const myFilter = (array: StudentsType[], callback: any) => {
-  const newArray = []
-  for (let i = 0; i < array.length; i++) {
-    if (callback(array[i])) {
-      newArray[i] = array[i]
-    }
-  }
-  return newArray
-}
+// import {StudentsType} from "./04.test";
+//
+// const ages = [22, 23, 45, 1, 91, 100]
+//
+// const predicat = (age: number) => {
+//   return age > 90
+// }
+// const oldAges = [100]
+//
+// export type CoursersType = {
+//   title: string
+//   price: number
+// }
+// let courses = [
+//   {title: "css", price: 100},
+//   {title: "JS", price: 200},
+//   {title: "React", price: 160},
+//   {title: "Python", price: 100}
+// ]
+//
+// export const myMap = (array: StudentsType[], callback: any) => {
+//   const newArray = []
+//   for (let i = 0; i < array.length; i++) {
+//     newArray[i] = callback(array[i])
+//   }
+//   return newArray
+// }
+// export const myFilter = (array: StudentsType[], callback: any) => {
+//   const newArray = []
+//   for (let i = 0; i < array.length; i++) {
+//     if (callback(array[i])) {
+//       newArray[i] = array[i]
+//     }
+//   }
+//   return newArray
+// }
+// export const myFind = (array: StudentsType[], callback: any) => {
+//   for (let i = 0; i < array.length; i++) {
+//     if (callback(array[i])) {
+//       return array[i]
+//     }
+//   }
+// }
+//
+//
+//
+//
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-
+type studentsType={
+  name:string
+  age:number
+  isMarried:boolean
+  score:number
+}
 function App() {
+  let[students,setStudents]=useState(
+   [
+    {name:'Andrey',age:43,isMarried:true,score:124},
+    {name:'Sergey',age:78,isMarried:false,score:100},
+    {name:'Bob',age:23,isMarried:true,score:89},
+    {name:'Tom',age:33,isMarried:false,score:99},
+    {name:'Mark',age:67,isMarried:true,score:12}
+  ])
+  function onClickHandler() {
+    let sortstudents=[...students].sort((a,b)=>b.name < a.name?-1:1).reverse()
+    setStudents([...sortstudents])
+  }
   return (
     <div className="App">
      Hey yo
+      {students.map(el=>{
+        return(
+          <div>
+            <hr/>
+            name: {el.name},
+            age: {el.age},
+            isMarried: {el.isMarried},
+            score: {el.score},
+            <hr/>
+          </div>
+        )
+      })}
+      <button onClick={onClickHandler}>Sort</button>
+
     </div>
   );
 }
