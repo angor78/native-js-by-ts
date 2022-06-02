545eb5b603e534e61e21973cfd9b90c2c3059710
Angor78 <angor78@gmail.com>
2022-06-02 12:38:47 +0300

    function deep copy and change

--- src/01/01.tsx
+++ src/01/01.tsx
@@ -1,21 +1,81 @@
-export function sum(a: number, b: number) {
-  return a + b
+export type MenPropsType = {
+  name: string
+  age: number
+}
+export type MotherPropsType = {
+  name: string
+  age: number
+}
+export const makeDeepCopySimpleObj = (obj: MenPropsType) => {
+  return {...obj}
 }
 
-export function mult(a: number, b: number) {
-  return a * b
+export type ObjInObjPropsType = MenPropsType & {
+  mother: MotherPropsType
+}
+export const makeCopyObjInObj = (obj: ObjInObjPropsType) => {
+  return {...obj, mother: {...obj.mother}}
+}
+export const changeMotherAge = (obj: ObjInObjPropsType, newAge: number) => {
+  return {...obj, mother: {...obj.mother, age: newAge}}
+}
+type SingleIngridientType = {
+  title: string
+  amount: number
+}
+type FavoriteDish = {
+  title: string
+  ingredients: Array<SingleIngridientType>
+}
+type ParentsType = {
+  name: string
+  age: number
+  favoriteDish: FavoriteDish
+}
+type WorkType = {
+  position: string
+  experience: number
+}
+type MotherType = {
+  name: string
+  age: number
+  work: WorkType
+  parents: Array<ParentsType>
 }
 
-export function removeChar(str: string) {
-  let newStr = str.slice(1, -1)
-  return newStr
-};
-
-
-export function splitIntoWords(sentense: string) {
-  return ['dsd', 'sasdas', 'vxcvxcv']
-
+export type makeDeepCopyManPropsType = {
+  name: string
+  age: number
+  mother: MotherType
 }
 
-//SPREAD OPER
-//TEST
+export const makeDeepCopyMan = (obj: makeDeepCopyManPropsType) => {
+  return {
+    ...obj,
+    mother: {
+      ...obj.mother, work: {...obj.mother.work},
+      parents: [...obj.mother.parents.map(el => el = {
+        ...el,
+        favoriteDish: {
+          ...el.favoriteDish,
+          ingredients: [...el.favoriteDish.ingredients.map(i => i = {...i})]
+        }
+      })]
+    }
+  }
+}
+export const changeIngredientsFavoriteDish = (obj: makeDeepCopyManPropsType,ingridient: string) => {
+  return {
+    ...obj,
+    mother: {
+      ...obj.mother, work: {...obj.mother.work},
+      parents: [...obj.mother.parents.map(el => el = {
+        ...el,
+        favoriteDish: {
+          ...el.favoriteDish,
+          ingredients: [...el.favoriteDish.ingredients.map(i =>el.favoriteDish.ingredients[0]?i = {...i,title:ingridient}:i={...i})]
+        }
+      })]
+    }
+  }
+}
