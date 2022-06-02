9c50ce55b75a3c372899d5e92a35240c309417e3
Angor78 <angor78@gmail.com>
2022-06-02 12:39:03 +0300

    jest tests deep copy and change

--- src/01/01.test.tsx
+++ src/01/01.test.tsx
@@ -1,27 +1,83 @@
-import {mult, removeChar, sum} from "./01";
+import {
+  changeIngredientsFavoriteDish,
+  changeMotherAge,
+  makeCopyObjInObj,
+  makeDeepCopyMan,
+  makeDeepCopySimpleObj
+} from "./01";
 
-let a: number
-let b: number
-let word: string
-beforeEach(() => {
-  a = 1
-  b = 2
-  word = 'sentense'
+
+test('deep copy simple',()=>{
+  let man = {
+    name: 'John',
+    age: 28
+  };
+
+  const manCopy = makeDeepCopySimpleObj(man)
+  expect(man.name).toBe(manCopy.name)
+  expect(man.age).toBe(manCopy.age)
 })
 
-test.skip('sum should be', () => {
+test('deep copy object inside an object',()=>{
+  let man1 = {
+    name: 'John',
+    age: 28,
+    mother: {
+      name: 'Kate',
+      age: 50
+    }
+  };
+  const copyMan = makeCopyObjInObj(man1)
+  const changedMan = changeMotherAge(man1,22)
 
-  const result = sum(a, b)
-  //expept result
-  expect(result).toBe(3)
+  expect(man1.name).toBe(copyMan.name)
+  expect(man1.mother.name).toBe(copyMan.mother.name)
+  expect(man1.mother.age).not.toBe(changedMan.mother.age)
+
 })
-test.skip('mult should be correct', () => {
 
-  const result = mult(a, b)
-  expect(result).toBe(2)
+test('deep copy and change nested objects',()=>{
+  let man = {
+    name: 'John',
+    age: 28,
+    mother: {
+      name: "Kate",
+      age: 50,
+      work: {
+        position: "doctor",
+        experience: 15
+      },
+      parents: [
+        {
+          name: "Kevin",
+          age: 80,
+          favoriteDish: {
+            title: "borscht",
+            ingredients: [
+              {title: "beet", amount: 3},
+              {title: "potatoes", amount: 5},
+              {title: "carrot", amount: 1},
+            ]
+          }
+        },
+        {
+          name: "Jennifer",
+          age: 78,
+          favoriteDish: {
+            title: "sushi",
+            ingredients: [
+              {title: "fish", amount: 1},
+              {title: "rise", amount: 0.5}
+            ]
+          }
+        },
+      ]
+    }
+  }
 
-})
-test.skip("removeChar should be remove first and last char from word", () => {
-  const splicer = removeChar(word)
-  expect(splicer).toBe("entens")
+  const deepCopyMan = makeDeepCopyMan(man)
+  expect(man.mother.parents[0].favoriteDish.ingredients[0].title).toBe(deepCopyMan.mother.parents[0].favoriteDish.ingredients[0].title)
+  const changedMan=changeIngredientsFavoriteDish(man,'pig')
+  expect(man.mother.parents[0].favoriteDish.ingredients[0].title).not.toBe(changedMan.mother.parents[0].favoriteDish.ingredients[0].title)
+
 })
